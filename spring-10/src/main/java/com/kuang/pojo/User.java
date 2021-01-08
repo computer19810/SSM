249aa2d88b483bb19e08b595ccf99756c566786f
computer19810 <computer19810@163.com>
2021-01-09 00:10:19 +0800

    20210109

---
+++ spring-10/src/main/java/com/kuang/pojo/User.java
@@ -0,0 +1,42 @@
+package com.kuang.pojo;
+
+public class User {
+
+    public int id;
+    public String name;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String pwd;
+}
